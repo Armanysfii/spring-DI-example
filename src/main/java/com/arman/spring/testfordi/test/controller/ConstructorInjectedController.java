2f131893cd59eb3c6099aac2b766e473b74439c4
arman <yousefiarman@yahoo.com>
2022-05-23 23:23:30 -0400

    Controllers

---
+++ src/main/java/com/arman/spring/testfordi/test/controller/ConstructorInjectedController.java
@@ -0,0 +1,18 @@
+package com.arman.spring.testfordi.test.controller;
+
+import com.arman.spring.testfordi.test.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/com/arman/spring/testfordi/test/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.arman.spring.testfordi.test.controller;
+
+import com.arman.spring.testfordi.test.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
