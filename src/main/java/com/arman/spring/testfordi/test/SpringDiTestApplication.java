cec61fda770e89e90b5825b1f6f170179684980d
arman <yousefiarman@yahoo.com>
2022-05-23 23:44:14 -0400

    Use of Profiles in SpringDiTestApplication

--- src/main/java/com/arman/spring/testfordi/test/SpringDiTestApplication.java
+++ src/main/java/com/arman/spring/testfordi/test/SpringDiTestApplication.java
@@ -1,9 +1,6 @@
 package com.arman.spring.testfordi.test;
 
-import com.arman.spring.testfordi.test.controller.ConstructorInjectedController;
-import com.arman.spring.testfordi.test.controller.MyController;
-import com.arman.spring.testfordi.test.controller.PropertyInjectedController;
-import com.arman.spring.testfordi.test.controller.SetterInjectedController;
+import com.arman.spring.testfordi.test.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SpringDiTestApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SpringDiTestApplication.class, args);
+
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+        System.out.println(i18nController.sayHello());
 
         MyController myController = (MyController) ctx.getBean("myController");
 
---
+++ src/main/java/com/arman/spring/testfordi/test/controller/I18nController.java
@@ -0,0 +1,21 @@
+package com.arman.spring.testfordi.test.controller;
+
+import com.arman.spring.testfordi.test.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class I18nController {
+
+    private final GreetingService greetingService;
+
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
+    }
+}
